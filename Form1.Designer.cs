aa4aff855afae537c9ded201ceebfb451c9dd884
boussaa.mehdi <boussaa.mehdi@INF-1013>
2026-05-11 09:00:39 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,399 @@
+﻿namespace Grafica_Gioco_del_impiccato
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            B = new Button();
+            C = new Button();
+            D = new Button();
+            E = new Button();
+            F = new Button();
+            G = new Button();
+            H = new Button();
+            I = new Button();
+            J = new Button();
+            K = new Button();
+            L = new Button();
+            M = new Button();
+            N = new Button();
+            O = new Button();
+            Q = new Button();
+            P = new Button();
+            R = new Button();
+            S = new Button();
+            T = new Button();
+            V = new Button();
+            U = new Button();
+            W = new Button();
+            Z = new Button();
+            Y = new Button();
+            X = new Button();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(26, 294);
+            button1.Name = "button1";
+            button1.Size = new Size(18, 23);
+            button1.TabIndex = 0;
+            button1.Text = "A";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(253, 73);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(240, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(137, 15);
+            label1.TabIndex = 2;
+            label1.Text = "GIOCO DELL'IMPICCATO";
+            label1.Click += label1_Click;
+            // 
+            // B
+            // 
+            B.Location = new Point(50, 294);
+            B.Name = "B";
+            B.Size = new Size(18, 23);
+            B.TabIndex = 3;
+            B.Text = "B";
+            B.UseVisualStyleBackColor = true;
+            // 
+            // C
+            // 
+            C.Location = new Point(74, 294);
+            C.Name = "C";
+            C.Size = new Size(18, 23);
+            C.TabIndex = 4;
+            C.Text = "C";
+            C.UseVisualStyleBackColor = true;
+            // 
+            // D
+            // 
+            D.Location = new Point(98, 294);
+            D.Name = "D";
+            D.Size = new Size(18, 23);
+            D.TabIndex = 5;
+            D.Text = "D";
+            D.UseVisualStyleBackColor = true;
+            // 
+            // E
+            // 
+            E.Location = new Point(122, 294);
+            E.Name = "E";
+            E.Size = new Size(18, 23);
+            E.TabIndex = 6;
+            E.Text = "E";
+            E.UseVisualStyleBackColor = true;
+            // 
+            // F
+            // 
+            F.Location = new Point(146, 294);
+            F.Name = "F";
+            F.Size = new Size(18, 23);
+            F.TabIndex = 7;
+            F.Text = "F";
+            F.UseVisualStyleBackColor = true;
+            // 
+            // G
+            // 
+            G.Location = new Point(170, 294);
+            G.Name = "G";
+            G.Size = new Size(18, 23);
+            G.TabIndex = 8;
+            G.Text = "G";
+            G.UseVisualStyleBackColor = true;
+            // 
+            // H
+            // 
+            H.Location = new Point(194, 294);
+            H.Name = "H";
+            H.Size = new Size(18, 23);
+            H.TabIndex = 9;
+            H.Text = "H";
+            H.UseVisualStyleBackColor = true;
+            // 
+            // I
+            // 
+            I.Location = new Point(218, 294);
+            I.Name = "I";
+            I.Size = new Size(18, 23);
+            I.TabIndex = 10;
+            I.Text = "I";
+            I.UseVisualStyleBackColor = true;
+            // 
+            // J
+            // 
+            J.Location = new Point(242, 294);
+            J.Name = "J";
+            J.Size = new Size(18, 23);
+            J.TabIndex = 11;
+            J.Text = "J";
+            J.UseVisualStyleBackColor = true;
+            // 
+            // K
+            // 
+            K.Location = new Point(266, 294);
+            K.Name = "K";
+            K.Size = new Size(18, 23);
+            K.TabIndex = 12;
+            K.Text = "K";
+            K.UseVisualStyleBackColor = true;
+            // 
+            // L
+            // 
+            L.Location = new Point(290, 294);
+            L.Name = "L";
+            L.Size = new Size(18, 23);
+            L.TabIndex = 13;
+            L.Text = "L";
+            L.UseVisualStyleBackColor = true;
+            // 
+            // M
+            // 
+            M.Location = new Point(314, 294);
+            M.Name = "M";
+            M.Size = new Size(18, 23);
+            M.TabIndex = 14;
+            M.Text = "M";
+            M.UseVisualStyleBackColor = true;
+            // 
+            // N
+            // 
+            N.Location = new Point(26, 342);
+            N.Name = "N";
+            N.Size = new Size(18, 23);
+            N.TabIndex = 15;
+            N.Text = "N";
+            N.UseVisualStyleBackColor = true;
+            // 
+            // O
+            // 
+            O.Location = new Point(50, 342);
+            O.Name = "O";
+            O.Size = new Size(18, 23);
+            O.TabIndex = 16;
+            O.Text = "O";
+            O.UseVisualStyleBackColor = true;
+            // 
+            // Q
+            // 
+            Q.Location = new Point(98, 342);
+            Q.Name = "Q";
+            Q.Size = new Size(18, 23);
+            Q.TabIndex = 17;
+            Q.Text = "Q";
+            Q.UseVisualStyleBackColor = true;
+            // 
+            // P
+            // 
+            P.Location = new Point(74, 342);
+            P.Name = "P";
+            P.Size = new Size(18, 23);
+            P.TabIndex = 18;
+            P.Text = "P";
+            P.UseVisualStyleBackColor = true;
+            // 
+            // R
+            // 
+            R.Location = new Point(122, 342);
+            R.Name = "R";
+            R.Size = new Size(18, 23);
+            R.TabIndex = 19;
+            R.Text = "R";
+            R.UseVisualStyleBackColor = true;
+            // 
+            // S
+            // 
+            S.Location = new Point(146, 342);
+            S.Name = "S";
+            S.Size = new Size(18, 23);
+            S.TabIndex = 20;
+            S.Text = "S";
+            S.UseVisualStyleBackColor = true;
+            // 
+            // T
+            // 
+            T.Location = new Point(170, 342);
+            T.Name = "T";
+            T.Size = new Size(18, 23);
+            T.TabIndex = 21;
+            T.Text = "T";
+            T.UseVisualStyleBackColor = true;
+            // 
+            // V
+            // 
+            V.Location = new Point(218, 342);
+            V.Name = "V";
+            V.Size = new Size(18, 23);
+            V.TabIndex = 22;
+            V.Text = "V";
+            V.UseVisualStyleBackColor = true;
+            // 
+            // U
+            // 
+            U.Location = new Point(194, 342);
+            U.Name = "U";
+            U.Size = new Size(18, 23);
+            U.TabIndex = 23;
+            U.Text = "U";
+            U.UseVisualStyleBackColor = true;
+            // 
+            // W
+            // 
+            W.Location = new Point(240, 342);
+            W.Name = "W";
+            W.Size = new Size(18, 23);
+            W.TabIndex = 24;
+            W.Text = "W";
+            W.UseVisualStyleBackColor = true;
+            // 
+            // Z
+            // 
+            Z.Location = new Point(314, 342);
+            Z.Name = "Z";
+            Z.Size = new Size(18, 23);
+            Z.TabIndex = 25;
+            Z.Text = "Z";
+            Z.UseVisualStyleBackColor = true;
+            // 
+            // Y
+            // 
+            Y.Location = new Point(290, 342);
+            Y.Name = "Y";
+            Y.Size = new Size(18, 23);
+            Y.TabIndex = 26;
+            Y.Text = "Y";
+            Y.UseVisualStyleBackColor = true;
+            // 
+            // X
+            // 
+            X.Location = new Point(266, 342);
+            X.Name = "X";
+            X.Size = new Size(18, 23);
+            X.TabIndex = 27;
+            X.Text = "X";
+            X.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(146, 264);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 15);
+            label2.TabIndex = 28;
+            label2.Text = "TASTIERA";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(X);
+            Controls.Add(Y);
+            Controls.Add(Z);
+            Controls.Add(W);
+            Controls.Add(U);
+            Controls.Add(V);
+            Controls.Add(T);
+            Controls.Add(S);
+            Controls.Add(R);
+            Controls.Add(P);
+            Controls.Add(Q);
+            Controls.Add(O);
+            Controls.Add(N);
+            Controls.Add(M);
+            Controls.Add(L);
+            Controls.Add(K);
+            Controls.Add(J);
+            Controls.Add(I);
+            Controls.Add(H);
+            Controls.Add(G);
+            Controls.Add(F);
+            Controls.Add(E);
+            Controls.Add(D);
+            Controls.Add(C);
+            Controls.Add(B);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Label label1;
+        private Button B;
+        private Button C;
+        private Button D;
+        private Button E;
+        private Button F;
+        private Button G;
+        private Button H;
+        private Button I;
+        private Button J;
+        private Button K;
+        private Button L;
+        private Button M;
+        private Button N;
+        private Button O;
+        private Button Q;
+        private Button P;
+        private Button R;
+        private Button S;
+        private Button T;
+        private Button V;
+        private Button U;
+        private Button W;
+        private Button Z;
+        private Button Y;
+        private Button X;
+        private Label label2;
+    }
+}
